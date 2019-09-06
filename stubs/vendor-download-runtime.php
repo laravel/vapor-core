0bb86d34cc1e1e374e771099f7e881e68a9a554a
Taylor Otwell <taylor@laravel.com>
2019-09-06 14:05:13 -0500

    change file

---
+++ stubs/vendor-download-runtime.php
@@ -0,0 +1,53 @@
+<?php
+
+$appRoot = $_ENV['LAMBDA_TASK_ROOT'];
+
+/*
+|--------------------------------------------------------------------------
+| Downlaod The Vendor Directory
+|--------------------------------------------------------------------------
+|
+| For applications which are loading their vendor directory on container
+| boot, we'll need to download it and extract it into place. This can
+| allow larger applications to be uploaded to Vapor without issues.
+|
+*/
+
+fwrite(STDERR, 'Downloading the application vendor archive...');
+
+exec(
+    sprintf('/opt/awscli/aws s3 presign s3://%s/%s-vendor.zip --expires-in 300',
+        $_ENV['VAPOR_ARTIFACT_BUCKET_NAME'],
+        $_ENV['VAPOR_ARTIFACT_NAME']
+    ),
+    $output
+);
+
+file_put_contents('/tmp/vendor.zip', fopen($output[0], 'r'));
+
+$zip = new ZipArchive;
+$zip->open('/tmp/vendor.zip');
+$zip->extractTo('/tmp/vendor');
+$zip->close();
+
+unlink('/tmp/vendor.zip');
+
+/*
+|--------------------------------------------------------------------------
+| Bootstrap The Runtime
+|--------------------------------------------------------------------------
+|
+| If the application is being served by the console layer, we will require in the
+| console runtime. Otherwise, we will use the FPM runtime. Vapor will setup an
+| environment variable for the console layer that we will use to check this.
+|
+*/
+
+require $appRoot.'/vendor/autoload.php';
+
+if (isset($_ENV['APP_RUNNING_IN_CONSOLE']) &&
+    $_ENV['APP_RUNNING_IN_CONSOLE'] === 'true') {
+    return require __DIR__.'/cliRuntime.php';
+} else {
+    return require __DIR__.'/fpmRuntime.php';
+}
