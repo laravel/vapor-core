d5bc54eeb52f96a87f6486babd9f913be5156071
Mohamed Said <themohamedsaid@gmail.com>
2019-10-25 13:00:41 +0200

    configure sqs

--- src/VaporServiceProvider.php
+++ src/VaporServiceProvider.php
@@ -12,7 +12,7 @@ use Laravel\Vapor\Http\Controllers\SignedStorageUrlController;
 
 class VaporServiceProvider extends ServiceProvider
 {
-    use ConfiguresDynamoDb, ConfiguresQueue, ConfiguresRedis, DefinesRoutes;
+    use ConfiguresDynamoDb, ConfiguresQueue, ConfiguresRedis, ConfiguresSQS, DefinesRoutes;
 
     /**
      * Bootstrap any application services.
@@ -65,6 +65,7 @@ class VaporServiceProvider extends ServiceProvider
         $this->ensureRedisIsConfigured();
         $this->ensureQueueIsConfigured();
         $this->ensureDynamoDbIsConfigured();
+        $this->ensureSQSIsConfigured();
         $this->ensureMixIsConfigured();
 
         $this->registerCommands();
