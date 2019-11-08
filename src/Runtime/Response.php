2272ec475573acfeca5f8dd48e8cee9881b7fd63
Taylor Otwell <taylor@laravel.com>
2019-11-08 07:16:52 -0600

    wip

--- src/Runtime/Response.php
+++ src/Runtime/Response.php
@@ -6,11 +6,17 @@ use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class Response
 {
+    /**
+     * Get the the status text for the given status code.
+     *
+     * @param  int  $status
+     * @return string
+     */
     public static function statusText($status)
     {
         $statusTexts = SymfonyResponse::$statusTexts;
 
-        $statusTexts[419] = 'Token expired';
+        $statusTexts[419] = 'Authentication Timeout';
 
         return $statusTexts[$status];
     }
