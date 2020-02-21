6ba383d9d243725330831ddc6c0b6b2e6d480211
Taylor Otwell <taylor@laravel.com>
2020-02-21 09:06:18 -0600

    Update VaporServiceProvider.php

--- src/VaporServiceProvider.php
+++ src/VaporServiceProvider.php
@@ -97,7 +97,7 @@ class VaporServiceProvider extends ServiceProvider
             return;
         }
 
-        $this->app[ConsoleKernel::class]->command('vapor:handle {payload}',function (){
+        $this->app[ConsoleKernel::class]->command('vapor:handle {payload}',function () {
             throw new InvalidArgumentException(
                 'Unknown event type. Please fine a vapor:handle command to handle custom events.'
             );
