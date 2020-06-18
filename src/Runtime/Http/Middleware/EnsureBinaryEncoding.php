9ceee1d3c5c631d2f8b8ce435bd812ac42463e3a
Taylor Otwell <taylorotwell@gmail.com>
2020-06-18 09:47:57 -0500

    formatting

--- src/Runtime/Http/Middleware/EnsureBinaryEncoding.php
+++ src/Runtime/Http/Middleware/EnsureBinaryEncoding.php
@@ -16,7 +16,6 @@ class EnsureBinaryEncoding
      */
     public function handle($request, $next)
     {
-        /** @var \Illuminate\Http\Response $response */
         $response = $next($request);
 
         if (static::isBase64EncodingRequired($response)) {
@@ -26,11 +25,18 @@ class EnsureBinaryEncoding
         return $response;
     }
 
+    /**
+     * Determine if base64 encoding is required for the response.
+     *
+     * @param  \Illuminate\Http\Response  $response
+     * @return bool
+     */
     public static function isBase64EncodingRequired(Response $response) : bool
     {
         $contentType = strtolower($response->headers->get('Content-Type', 'text/html'));
 
-        if (Str::startsWith($contentType, 'text/') || Str::contains($contentType, ['xml', 'json'])) {
+        if (Str::startsWith($contentType, 'text/') ||
+            Str::contains($contentType, ['xml', 'json'])) {
             return false;
         }
 
