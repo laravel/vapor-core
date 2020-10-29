968fe9442dbd2e1e889183120f661d37ede97d01
Nuno Maduro <enunomaduro@gmail.com>
2020-10-29 19:50:08 +0100

    Adds support to PHP 8 (#68)
    * Adds support to PHP 8
    
    * Removes ignore platform req
    
    * Adds PHP 8 to CI workflow
    
    * Updates workflow

--- tests/Unit/HttpKernelTest.php
+++ tests/Unit/HttpKernelTest.php
@@ -16,21 +16,6 @@ class HttpKernelTest extends TestCase
         Mockery::close();
     }
 
-    public function test_request_can_be_handled()
-    {
-        // $app = Mockery::mock('Illuminate\Foundation\Application');
-
-        // $app->shouldReceive('useStoragePath')->once()->with('/tmp/storage');
-        // $app->shouldReceive('storagePath')->andReturn('/tmp/storage');
-        // $app->shouldReceive('handle')->andReturn($mockResponse = new Response('Hello World'));
-        // $app->shouldReceive('terminate')->once();
-
-        // $handler = new HttpKernel($app);
-        // $response = $handler->handle(Request::create('/', 'GET'));
-
-        // $this->assertEquals($mockResponse, $response);
-    }
-
     public function test_should_send_maintenance_mode_response_when_enabled_and_on_non_vanity_domain()
     {
         $_ENV['APP_VANITY_URL'] = 'https://something.com';
