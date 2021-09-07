594879d6402543f4e644e95f69a16b152d3f6595
Taylor Otwell <taylorotwell@users.noreply.github.com>
2021-09-07 11:54:23 +0000

    Apply fixes from StyleCI

--- src/Runtime/HttpKernel.php
+++ src/Runtime/HttpKernel.php
@@ -55,7 +55,6 @@ class HttpKernel
                 $this->hasValidBypassCookie($request, $_ENV['VAPOR_MAINTENANCE_MODE_SECRET'])) {
                 $response = $this->sendRequest($request);
             } else {
-
                 if ($request->wantsJson() && file_exists($_ENV['LAMBDA_TASK_ROOT'].'/503.json')) {
                     $response = JsonResponse::fromJsonString(
                         file_get_contents($_ENV['LAMBDA_TASK_ROOT'].'/503.json'), 503
