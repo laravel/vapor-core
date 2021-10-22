511b9c5a837f7baf3a0edf0ea7db464efaca6dea
Eduardo Agostini <edu.agostini@gmail.com>
2021-10-23 02:12:15 +1300

    Signed Storage URLs → Scheme & Port (#106)
    - Replacing hardcoded `https` scheme with the same one coming from the previously generated URI.
    - Replacing usage of `$uri->getHost()` with `$uri->getAuthority()` to include the port coming from the previously generated URI.

--- src/Http/Controllers/SignedStorageUrlController.php
+++ src/Http/Controllers/SignedStorageUrlController.php
@@ -42,7 +42,7 @@ class SignedStorageUrlController extends Controller implements SignedStorageUrlC
             'uuid' => $uuid,
             'bucket' => $bucket,
             'key' => $key,
-            'url' => 'https://'.$uri->getHost().$uri->getPath().'?'.$uri->getQuery(),
+            'url' => $uri->getScheme().'://'.$uri->getAuthority().$uri->getPath().'?'.$uri->getQuery(),
             'headers' => $this->headers($request, $signedRequest),
         ], 201);
     }
---
+++ tests/Feature/SignedStorageUrlEndpointTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Laravel\Vapor\Tests\Feature;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Vapor\VaporServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class SignedStorageUrlEndpointTest extends TestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            VaporServiceProvider::class,
+        ];
+    }
+
+    public function test_signed_url_preserves_its_components(): void
+    {
+        Config::set([
+            'filesystems.disks.s3.bucket' => $_ENV['AWS_BUCKET'] = 'storage',
+            'filesystems.disks.s3.key' => $_ENV['AWS_ACCESS_KEY_ID'] = 'sail',
+            'filesystems.disks.s3.region' => $_ENV['AWS_DEFAULT_REGION'] = 'us-east-1',
+            'filesystems.disks.s3.secret' => $_ENV['AWS_SECRET_ACCESS_KEY'] = 'password',
+            'filesystems.disks.s3.url' => $_ENV['AWS_URL'] = 'http://minio:9000',
+            'filesystems.disks.s3.use_path_style_endpoint' => true,
+        ]);
+
+        Gate::define('uploadFiles', static function ($user = null, $bucket = null): bool {
+            return true;
+        });
+
+        $response = $this->json('POST', '/vapor/signed-storage-url')
+            ->assertStatus(201);
+
+        $components = (object) [
+            'actual' => parse_url($response->json('url')),
+            'expected' => parse_url($_ENV['AWS_URL'].'/'.$_ENV['AWS_BUCKET']),
+        ];
+
+        $this->assertEquals($components->expected['scheme'], $components->actual['scheme']);
+        $this->assertEquals($components->expected['host'], $components->actual['host']);
+        $this->assertEquals($components->expected['port'], $components->actual['port']);
+        $this->assertStringStartsWith($components->expected['path'], $components->actual['path']);
+    }
+}
