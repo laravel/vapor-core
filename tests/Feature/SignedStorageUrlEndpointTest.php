ce1e96abd777343d7ba05c55559510fb13d2e338
Nuno Maduro <enunomaduro@gmail.com>
2021-11-09 14:30:12 +0000

    Adds `vapor.signed_url_expires_after` (#110)
    * Adds `vapor.signed_url_expires_after`
    
    * Removes trailing comma
    
    * Update SignedStorageUrlController.php
    
    * Update SignedStorageUrlEndpointTest.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- tests/Feature/SignedStorageUrlEndpointTest.php
+++ tests/Feature/SignedStorageUrlEndpointTest.php
@@ -44,4 +44,29 @@ class SignedStorageUrlEndpointTest extends TestCase
         $this->assertEquals($components->expected['port'], $components->actual['port']);
         $this->assertStringStartsWith($components->expected['path'], $components->actual['path']);
     }
+
+    public function test_signed_url_expires_after(): void
+    {
+        Config::set([
+            'filesystems.disks.s3.bucket' => $_ENV['AWS_BUCKET'] = 'storage',
+            'filesystems.disks.s3.key' => $_ENV['AWS_ACCESS_KEY_ID'] = 'sail',
+            'filesystems.disks.s3.region' => $_ENV['AWS_DEFAULT_REGION'] = 'us-east-1',
+            'filesystems.disks.s3.secret' => $_ENV['AWS_SECRET_ACCESS_KEY'] = 'password',
+            'filesystems.disks.s3.url' => $_ENV['AWS_URL'] = 'http://minio:9000',
+            'filesystems.disks.s3.use_path_style_endpoint' => true,
+        ]);
+
+        Gate::define('uploadFiles', static function ($user = null, $bucket = null): bool {
+            return true;
+        });
+
+        $response = $this->json('POST', '/vapor/signed-storage-url');
+        parse_str($response->json()['url'], $queryParams);
+        $this->assertEquals(300, $queryParams['X-Amz-Expires']);
+
+        config()->set('vapor.signed_storage_url_expires_after', 6);
+        $response = $this->json('POST', '/vapor/signed-storage-url');
+        parse_str($response->json()['url'], $queryParams);
+        $this->assertEquals(360, $queryParams['X-Amz-Expires']);
+    }
 }
