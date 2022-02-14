517704ac71aa15ad26687fd1ffbc0d0e1919cc49
Nuno Maduro <enunomaduro@gmail.com>
2022-02-14 17:02:28 +0000

    Allows to disable redirect to `favicon.ico` logic (#121)
    * Allows to disable `redirect_favicon_ico`
    
    * Update RedirectStaticAssets.php
    
    * Update RedirectStaticAssetsTest.php
    
    Co-authored-by: Taylor Otwell <taylorotwell@gmail.com>

---
+++ tests/Feature/RedirectStaticAssetsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Laravel\Vapor\Tests\Feature;
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Vapor\Runtime\Http\Middleware\RedirectStaticAssets;
+use Laravel\Vapor\VaporServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class RedirectStaticAssetsTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $_ENV['ASSET_URL'] = 'https://asset-url.com';
+
+        Route::get('/favicon.ico', function () {
+            return 'My own favicon.';
+        })->middleware(RedirectStaticAssets::class);
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            VaporServiceProvider::class,
+        ];
+    }
+
+    public function test_redirects_to_favicon_ico()
+    {
+        $response = $this->get('/favicon.ico');
+        $response->assertStatus(302)->assertRedirect('https://asset-url.com/favicon.ico');
+
+        config()->set('vapor.redirect_favicon', false);
+
+        $response = $this->get('/favicon.ico');
+        $response->assertStatus(200)->assertSee('My own favicon.');
+    }
+}
