118665095f76e29fe7c5c8231c3caec1fb64a199
Miloš Gavrilović <milos@gavrilovic.rs>
2022-03-10 17:39:27 +0100

    make defining routes configurable (#125)
    * make defining routes configurable
    
    Signed-off-by: Miloš Gavrilović <milos@gavrilovic.rs>
    
    * rename config for enabling signed storage route
    
    Signed-off-by: Miloš Gavrilović <milos@gavrilovic.rs>
    
    * make signed storage url configurable
    
    Signed-off-by: Miloš Gavrilović <milos@gavrilovic.rs>

--- src/DefinesRoutes.php
+++ src/DefinesRoutes.php
@@ -17,9 +17,11 @@ trait DefinesRoutes
             return;
         }
 
-        Route::post(
-            '/vapor/signed-storage-url',
-            Contracts\SignedStorageUrlController::class.'@store'
-        )->middleware(config('vapor.middleware', 'web'));
+        if (config('vapor.signed_storage.enabled', true)) {
+            Route::post(
+                config('vapor.signed_storage.url', '/vapor/signed-storage-url'),
+                Contracts\SignedStorageUrlController::class.'@store'
+            )->middleware(config('vapor.middleware', 'web'));
+        }
     }
 }
