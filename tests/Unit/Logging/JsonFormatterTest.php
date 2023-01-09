9d7f8f419cb7989d2811e46c4e8d2e904bbbcec4
Nuno Maduro <enunomaduro@gmail.com>
2023-01-09 16:30:39 +0000

    Improves tests around monolog

--- tests/Unit/Logging/JsonFormatterTest.php
+++ tests/Unit/Logging/JsonFormatterTest.php
@@ -17,6 +17,71 @@ class JsonFormatterTest extends TestCase
         unset($_ENV['AWS_REQUEST_ID']);
     }
 
+    public function test_format()
+    {
+        $formatter = new JsonFormatter();
+
+        if (class_exists(LogRecord::class)) {
+            $record = new LogRecord(
+                new \DateTimeImmutable(),
+                'channel-test',
+                Level::Info,
+                'message-test',
+                ['context-test' => 'context-value'],
+                ['extra-test' => 'extra-value']
+            );
+        } else {
+            $record = [
+                'datetime' => new \DateTimeImmutable(),
+                'channel' => 'channel-test',
+                'level' => 200,
+                'level_name' => 'INFO',
+                'message' => 'message-test',
+                'context' => ['context-test' => 'context-value'],
+                'extra' => ['extra-test' => 'extra-value'],
+            ];
+        }
+
+        $_ENV['AWS_REQUEST_ID'] = '123456789';
+
+        $record = $formatter->format($record);
+        $this->assertJson($record);
+
+        $record = json_decode($record, true);
+
+        if (class_exists(LogRecord::class)) {
+            $this->assertSame([
+                'message' => 'message-test',
+                'context' => [
+                    'context-test' => 'context-value',
+                    'aws_request_id' => '123456789',
+                ],
+                'level' => 200,
+                'level_name' => 'INFO',
+                'channel' => 'channel-test',
+                'datetime' => $record['datetime'],
+                'extra' => [
+                    'extra-test' => 'extra-value',
+                ],
+            ], $record);
+        } else {
+            $this->assertSame([
+                'datetime' => $record['datetime'],
+                'channel' => 'channel-test',
+                'level' => 200,
+                'level_name' => 'INFO',
+                'message' => 'message-test',
+                'context' => [
+                    'context-test' => 'context-value',
+                    'aws_request_id' => '123456789',
+                ],
+                'extra' => [
+                    'extra-test' => 'extra-value',
+                ],
+            ], $record);
+        }
+    }
+
     public function test_includes_aws_request_id()
     {
         $formatter = new JsonFormatter();
@@ -33,12 +98,12 @@ class JsonFormatterTest extends TestCase
             );
         } else {
             $record = [
-                'message' => 'test',
-                'context' => ['foo' => 'bar'],
+                'datetime' => new \DateTimeImmutable(),
+                'channel' => 'channel-test',
                 'level' => 200,
                 'level_name' => 'INFO',
-                'channel' => 'test',
-                'datetime' => new \DateTimeImmutable(),
+                'message' => 'message-test',
+                'context' => ['foo' => 'bar'],
                 'extra' => [],
             ];
         }
@@ -63,12 +128,12 @@ class JsonFormatterTest extends TestCase
             );
         } else {
             $record = [
-                'message' => 'test',
-                'context' => ['foo' => 'bar'],
+                'datetime' => new \DateTimeImmutable(),
+                'channel' => 'channel-test',
                 'level' => 200,
                 'level_name' => 'INFO',
-                'channel' => 'test',
-                'datetime' => new \DateTimeImmutable(),
+                'message' => 'message-test',
+                'context' => [],
                 'extra' => [],
             ];
         }
