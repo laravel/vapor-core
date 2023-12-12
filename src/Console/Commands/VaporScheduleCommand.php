9eb617fda24dfd4a9f77985960844b79306f0593
Joe Dixon <hello@joedixon.co.uk>
2023-12-12 16:50:46 +0000

    add vapor scheduler proxy

---
+++ src/Console/Commands/VaporScheduleCommand.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Laravel\Vapor\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Support\Str;
+
+class VaporScheduleCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'vapor:schedule';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run a more granular scheduler for Vapor';
+
+    /**
+     * Indicates whether the command should be shown in the Artisan command list.
+     *
+     * @var bool
+     */
+    protected $hidden = true;
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (! $cache = $this->ensureValidCacheDriver()) {
+            $this->call('schedule:run');
+
+            return self::SUCCESS;
+        }
+
+        $key = (string) Str::uuid();
+        $lockObtained = false;
+
+        while (true) {
+            if (! $lockObtained) {
+                $lockObtained = $this->obtainLock($cache, $key);
+            }
+
+            if ($lockObtained && now()->second === 0) {
+                $this->call('schedule:run');
+
+                $this->releaseLock($cache);
+
+                return self::SUCCESS;
+            }
+
+            usleep(10000);
+        }
+    }
+
+    /**
+     * Ensure the cache driver is valid.
+     */
+    protected function ensureValidCacheDriver(): ?Repository
+    {
+        $manager = $this->laravel['cache'];
+
+        if (in_array($manager->getDefaultDriver(), ['memcached', 'redis', 'dynamodb', 'database'])) {
+            return $manager->driver();
+        }
+    }
+
+    /**
+     * Obtain the lock for the schedule.
+     */
+    protected function obtainLock(Repository $cache, string $key): bool
+    {
+        return $key === $cache->remember('vapor:schedule:lock', 60, fn () => $key);
+    }
+
+    /**
+     * Release the lock for the schedule.
+     *
+     * @param  string  $key
+     */
+    protected function releaseLock(Repository $cache): void
+    {
+        $cache->forget('vapor:schedule:lock');
+    }
+}
