ee26b22f54d13baea274be5001fb09b18cea1ea6
Sven <Sfonxs@users.noreply.github.com>
2026-01-08 17:22:47 +0100

    Add dynamic cli handler factory. (#197)
    * Add dynamic cli handler factory.
    
    * Return the handlers instead of boolean.
    
    ---------
    
    Co-authored-by: Sven Dedeene <sven.dedeeene@gmail.com>

--- src/Runtime/CliHandlerFactory.php
+++ src/Runtime/CliHandlerFactory.php
@@ -8,6 +8,13 @@ use Laravel\Vapor\Runtime\Handlers\QueueHandler;
 class CliHandlerFactory
 {
     /**
+     * The custom handler factory callback.
+     *
+     * @var callable|null
+     */
+    protected static $customHandlerFactory;
+
+    /**
      * Create a new handler for the given CLI event.
      *
      * @param  array  $event
@@ -15,6 +22,10 @@ class CliHandlerFactory
      */
     public static function make(array $event)
     {
+        if (static::$customHandlerFactory) {
+            return call_user_func(static::$customHandlerFactory, $event);
+        }
+
         $messageId = $event['Records'][0]['messageId'] ?? null;
 
         $job = json_decode($event['Records'][0]['body'] ?? '')->job ?? null;
@@ -22,5 +33,26 @@ class CliHandlerFactory
         return $messageId && $job
                     ? new QueueHandler
                     : new CliHandler;
+    }
+
+    /**
+     * Set a custom handler factory callback.
+     *
+     * @param  callable  $callback
+     * @return void
+     */
+    public static function createHandlerUsing(callable $callback)
+    {
+        static::$customHandlerFactory = $callback;
+    }
+
+    /**
+     * Reset the handler factory to its default behavior.
+     *
+     * @return void
+     */
+    public static function createHandlersNormally()
+    {
+        static::$customHandlerFactory = null;
     }
 }
--- tests/Unit/Runtime/CliHandlerFactoryTest.php
+++ tests/Unit/Runtime/CliHandlerFactoryTest.php
@@ -16,6 +16,13 @@ class CliHandlerFactoryTest extends TestCase
         QueueHandler::$app = 'dummy';
     }
 
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        CliHandlerFactory::createHandlersNormally();
+    }
+
     public function test_custom_sqs_events_use_cli_handler()
     {
         $this->assertInstanceOf(CliHandler::class, CliHandlerFactory::make($this->getSQSCustomEvent()));
@@ -28,6 +35,42 @@ class CliHandlerFactoryTest extends TestCase
 
     public function test_laravel_jobs_use_queue_handler()
     {
+        $this->assertInstanceOf(QueueHandler::class, CliHandlerFactory::make($this->getSQSJobEvent()));
+    }
+
+    public function test_custom_handler_factory_is_used_when_set()
+    {
+        CliHandlerFactory::createHandlerUsing(function ($event) {
+            return new CliHandler;
+        });
+
+        $this->assertInstanceOf(CliHandler::class, CliHandlerFactory::make($this->getSQSJobEvent()));
+    }
+
+    public function test_custom_handler_factory_receives_event()
+    {
+        $receivedEvent = null;
+
+        CliHandlerFactory::createHandlerUsing(function ($event) use (&$receivedEvent) {
+            $receivedEvent = $event;
+
+            return new CliHandler;
+        });
+
+        $expectedEvent = $this->getSQSJobEvent();
+        CliHandlerFactory::make($expectedEvent);
+
+        $this->assertSame($expectedEvent, $receivedEvent);
+    }
+
+    public function test_default_behavior_is_restored_after_reset()
+    {
+        CliHandlerFactory::createHandlerUsing(function ($event) {
+            return new CliHandler;
+        });
+
+        CliHandlerFactory::createHandlersNormally();
+
         $this->assertInstanceOf(QueueHandler::class, CliHandlerFactory::make($this->getSQSJobEvent()));
     }
 
