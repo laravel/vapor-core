03912cabcc7d52785d3f4638c24ee77a401055cc
Taylor Otwell <taylor@laravel.com>
2026-08-04 09:12:05 -0500

    Update CliHandlerFactory.php

--- src/Runtime/CliHandlerFactory.php
+++ src/Runtime/CliHandlerFactory.php
@@ -30,7 +30,7 @@ class CliHandlerFactory
 
         $body = json_decode($event['Records'][0]['body'] ?? '');
 
-        // SQS payload overflow replaces the message body with a pointer to the stored payload
+        // SQS payload overflow replaces the message body with a pointer to the stored payload...
         $job = $body->job ?? $body->{'@pointer'} ?? null;
 
         return $messageId && $job
